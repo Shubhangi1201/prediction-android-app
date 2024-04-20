cea6df998f5e8761b472c1a4565430171e5a844d
anonymous <itisme1412@gmail.com>
2024-04-20 11:54:44 +0530

    dfjs

--- app/src/main/java/com/example/turnoverprediction/PredictionActivity.kt
+++ app/src/main/java/com/example/turnoverprediction/PredictionActivity.kt
@@ -118,10 +118,8 @@ class PredictionActivity : AppCompatActivity() {
         val highSatisfactionLevel = 0.5
         val lowLastEvaluation = 0.5
         val manyProjects = 3
-        val highAverageMonthlyHours = 100
+        val highAverageMonthlyHours = 150
         val longTimeSpentCompany = 5
-        val workAccidentWeight = 0.2
-        val promotionWeight = 0.1
 
 
         // Convert the input parameters to the appropriate data types
@@ -130,13 +128,13 @@ class PredictionActivity : AppCompatActivity() {
         val numberOfProjectsValue = numberOfProjects.toIntOrNull() ?: 0
         val averageMonthlyHoursValue = averageMonthlyHours.toDoubleOrNull() ?: 0.0
         val timeSpentCompanyValue = timeSpentCompany.toIntOrNull() ?: 0
-//        val score =
-//            if (satisfactionLevelValue > highSatisfactionLevel) 0.5 else 0.0 + if (lastEvaluationValue < lowLastEvaluation) 0.5 else 0.0 + if (numberOfProjectsValue > manyProjects) 0.5 else 0.0 + if (averageMonthlyHoursValue > highAverageMonthlyHours) 0.5 else 0.0 + if (timeSpentCompanyValue > longTimeSpentCompany) 0.5 else 0.0 + if (workAccident) workAccidentWeight else 0.0 + if (promotionInLastFiveYears) -promotionWeight else 0.0
 
-        Toast.makeText(applicationContext, "inside predict function", Toast.LENGTH_SHORT).show()
 
-        // Return a prediction based on the score
-        return true
+        if(satisfactionLevelValue<highSatisfactionLevel && lastEvaluationValue<lowLastEvaluation && numberOfProjectsValue>manyProjects && averageMonthlyHoursValue>highAverageMonthlyHours && workAccident && !promotionInLastFiveYears){
+            return true
+        }else{
+            return false
+        }
 
     }
 
